d943cfeeb5e6e62baddff2f181cc770b165da2eb
poi80966 <128391184+poi80966@users.noreply.github.com>
2026-04-20 02:51:01 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "**.replicate.delivery" },
+      { protocol: "https", hostname: "replicate.delivery" },
+    ],
+  },
+}
 module.exports = nextConfig
